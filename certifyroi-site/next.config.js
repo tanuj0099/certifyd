0efd937a24da3566a9950dc2f1da8defb9d3a6d7
Tanuj Rajdev <tanuj.rajdev@gmail.com>
2026-06-02 15:17:17 +0530

    idk bro just give up

--- certifyroi-site/next.config.js
+++ certifyroi-site/next.config.js
@@ -1,10 +1,13 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Config options here
-}
-
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
+  },
+};
+export default nextConfig;
 module.exports = withBundleAnalyzer(nextConfig)
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '5mb',
+    },
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
