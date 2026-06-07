444ea0b48d040b3da0decbcf3b762f7db6fa4b96
Tanuj Rajdev <tanuj.rajdev@gmail.com>
2026-06-07 17:41:57 +0530

    style(offer-analysis): remove max-width constraint to utilize full screen space

--- src/app/offer-analysis/page.jsx
+++ src/app/offer-analysis/page.jsx
@@ -332,7 +332,7 @@ export default function OfferAnalysisPage() {
       footer={false}
       showFeedback={false}
     >
-      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
 
         {/* ROI-style Flow Stepper */}
         {!hasResult && (
