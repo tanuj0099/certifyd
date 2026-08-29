d6865cdaef5b0900d324b938f7b89c95c9e62282
Tanuj Rajdev <tanuj.rajdev@gmail.com>
2026-08-30 00:13:05 +0530

    fix: map private env vars to public ones to bypass Vercel restrictions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['172.16.0.2'],
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 export default nextConfig;
